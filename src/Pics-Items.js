f3b06de6a33add545047e06c80a8b5e99236a1c0
Samuel S. Siegmeister <siegmeister.s@husky.neu.edu>
2020-09-15 11:58:25 -0400

    more photos

--- src/Pics-Items.js
+++ src/Pics-Items.js
@@ -1,13 +1,16 @@
-import alley from './images/alley.jpeg';
+import bikes from './images/bikes.jpg';
 import bridge from './images/bridge.jpeg';
 import charles from './images/charles.jpeg';
 import dark from './images/dark.jpeg';
+import glass from './images/glass.jpg';
 import light from './images/light.jpeg';
 import mountain from './images/mountain.jpeg';
 import sunrise from './images/sunrise.jpeg';
 import tower from './images/tower.jpeg';
 import train from './images/train.jpeg';
+import tree from './images/tree.jpg';
+import watertown from './images/watertown.jpg';
 
-const pics = [alley, bridge, charles, dark, light, mountain, sunrise, tower, train];
+const pics = [bikes, bridge, charles, dark, light, glass, mountain, sunrise, tower, train, tree, watertown];
 
 export default pics;
